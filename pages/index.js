71b75f47c84d847b26bdfe32fd67969323536926
Ahmed-Basuony <ahmed.basouny@awstreams.com>
2022-10-07 01:10:11 +0200

    Fix apiVersion data format && create request to fetch sanity data with getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,36 @@
 import React from 'react'
 import { Banner, Footer } from '../components'
+import { client } from '../lib/client';
 
 
-const index = () => {
+export default  function index ({products, bannerData}) {
+  console.log(bannerData)
   return (
     <>
-      <Banner />
+      <Banner heroBanner={bannerData.length && bannerData[0]} />
       <div className='products-heading'>
         <h2>Best Selling Ever</h2>
         <p>There are may variations passages</p>
       </div>
-      <div className='prodcuts-container'>products</div>
+      <div className='prodcuts-container'>{products?.map(product => product.name)}</div>
       <Footer />
     </>
   )
 }
 
-export default index
+
+export async function getServerSideProps() {
+  const query = '*[_type == "product"]';
+  const productsData = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { 
+      products: productsData,
+      bannerData
+     }
+  }
+
+}
