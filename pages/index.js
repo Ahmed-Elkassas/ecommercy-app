016c44e1b83ec1c8ceb413c938e313966ccdd0a3
Ahmed-Basuony <ahmed.basouny@awstreams.com>
2022-10-07 02:28:38 +0200

    Add banner & products data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Banner, Footer } from '../components'
+import Product from '../components/Product';
 import { client } from '../lib/client';
 
 
 export default  function index ({products, bannerData}) {
-  console.log(bannerData)
   return (
     <>
       <Banner heroBanner={bannerData.length && bannerData[0]} />
@@ -12,7 +12,7 @@ export default  function index ({products, bannerData}) {
         <h2>Best Selling Ever</h2>
         <p>There are may variations passages</p>
       </div>
-      <div className='prodcuts-container'>{products?.map(product => product.name)}</div>
+      <div className='products-container'>{products?.map(product => <Product key={product._id} product={product} />)}</div>
       <Footer />
     </>
   )
